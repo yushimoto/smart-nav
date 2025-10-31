7f26fbdb70e33940a9ef536fc9e08075dca21438
Akshay Daundkar <akshaydaundkar01@gmail.com>
2025-10-31 11:52:14 -0500

    changed config

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,27 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.hashnode.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn.hashnode.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'hashnode.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "hashnode.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.hashnode.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.hashnode.com",
+        port: "",
+        pathname: "/**",
       },
     ],
+    unoptimized: true,
   },
+  output: "export",
 };
 
 export default nextConfig;
